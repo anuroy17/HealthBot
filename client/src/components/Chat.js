995fd9dfdf17659f4770ea6f6d3a5c9b254b7180
Deivanai <deivanai0304@gmail.com>
2023-10-24 21:29:12 +0530

    Made connection to rasa model and react

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Chat.css"
 
 import Box from '@mui/material/Box';
@@ -14,10 +14,90 @@ import VaccinesIcon from '@mui/icons-material/Vaccines';
 import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 import SendIcon from '@mui/icons-material/Send';
-import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import Avatar from '@mui/material/Avatar';
+
 
 
 const Chat = () => {
+
+  const [chat,setChat] = useState([]);
+    const [inputMessage,setInputMessage] = useState('');
+    const [botTyping,setbotTyping] = useState(false);
+
+    
+   useEffect(()=>{
+   
+        console.log("called");
+        const objDiv = document.getElementById('messageArea');
+        objDiv.scrollTop = objDiv.scrollHeight;
+        
+    
+    },[chat])
+
+    
+
+
+    const handleSubmit=(evt)=>{
+        evt.preventDefault();
+        const name = "shreyas";
+        const request_temp = {sender : "user", sender_id : name , msg : inputMessage};
+        
+        if(inputMessage !== ""){
+            
+            setChat(chat => [...chat, request_temp]);
+            setbotTyping(true);
+            setInputMessage('');
+            rasaAPI(name,inputMessage);
+        }
+        else{
+            window.alert("Please enter valid message");
+        }
+        
+    }
+
+    const rasaAPI = async function handleClick(name,msg) {
+    
+      //chatData.push({sender : "user", sender_id : name, msg : msg});
+      
+
+        await fetch('http://localhost:5005/webhooks/rest/webhook', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'charset':'UTF-8',
+          },
+          credentials: "same-origin",
+          body: JSON.stringify({ "sender": name, "message": msg }),
+      })
+      .then(response => response.json())
+      .then((response) => {
+          if(response){
+              const temp = response[0];
+              const recipient_id = temp["recipient_id"];
+              const recipient_msg = temp["text"];        
+
+
+              const response_temp = {sender: "bot",recipient_id : recipient_id,msg: recipient_msg};
+              setbotTyping(false);
+              
+              setChat(chat => [...chat, response_temp]);
+             // scrollBottom();
+
+          }
+      }) 
+  }
+
+  console.log(chat);
+
+  const styleBody = {
+    paddingTop : '10px',
+    height: '28rem',
+    overflowY: 'a',
+    overflowX: 'hidden',
+    
+}
+
   return (
     <div className='main'>
       <div className='nav'>
@@ -71,17 +151,39 @@ const Chat = () => {
         <div className='head'>
           <div className='boticon'><SmartToyIcon sx={{ fontSize: 40 }}/></div>
           <h1>HealthBot</h1>
+          {botTyping ? <h6>Bot Typing....</h6> : <h6>bot not typing</h6>}
         </div>
-        <div className='chat'>
 
+
+        <div className="cardBody" id="messageArea" style={styleBody}>
+          <div className="row msgarea">
+            {chat.map((user,key) => (
+            <div key={key}>
+              {user.sender==='bot' ?
+              (
+              <div className= 'msgalignstart'>
+                <SmartToyIcon/><h5 className="botmsg">{user.msg}</h5>
+              </div>
+              ):
+              (
+              <div className= 'msgalignend'>
+                <h5 className="usermsg">{user.msg}</h5><Avatar style={{background: "#53A57D"}}/>
+              </div>
+              )
+              }
+            </div>))}
+          </div>
         </div>
+
+
         <div className='message'>
-        <div className='input'><input type="text"  name="message" id="message" placeholder='Type your query' /></div>
+        <form style={{display: 'flex'}} onSubmit={handleSubmit}>
+        <div className='input'><input onChange={e => setInputMessage(e.target.value)} value={inputMessage} type="text" className="msginp"></input></div>
         
         <div className='out'>
-        <button className='btn' ><KeyboardVoiceIcon/>Record</button>
-        <button className='btn' >Send  <SendIcon/></button>
+        <button className='btn' type='submit' >Send  <SendIcon/></button>
         </div>
+        </form>
         </div>
       </div>
     </div>
